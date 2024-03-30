121913e0d5da5be1a6eef889f67c787718c159c4
rafael gildin <rafael.gildin@pmygroup.com>
2024-03-30 18:06:39 -0300

    add arduino_video_inference

--- inference.py
+++ inference.py
@@ -1,59 +1,4 @@
-from IPython.display import display
-from PIL import Image
-from ultralytics import YOLO
-import cv2 as cv
-import os, serial, time
-import keyboard # get the keys typed
-
-def green_led_on(): ser.write(b'G')
-def green_led_off(): ser.write(b'g')
-def yellow_led_on(): ser.write(b'Y')
-def yellow_led_off(): ser.write(b'y')
-def blink():
-    print("yes detected")
-    green_led_off()
-    yellow_led_on()
-    time.sleep(2)
-    yellow_led_off()
-    green_led_on()
-        
-def plot_frame(frame,width,height,text):
-    # Define the position and size of the rectangle
-    top_left_corner = (0, 0)
-    bottom_right_corner = (width, 100)
-    rectangle_color = (255, 255, 255)  # White color in BGR
-    rectangle_thickness = -1  # Filled rectangle
-
-    # Draw the white rectangle
-    cv.rectangle(frame, top_left_corner, bottom_right_corner, rectangle_color, rectangle_thickness)
-
-    # Write text on the frame using OpenCV before converting it to RGB
-    font = cv.FONT_HERSHEY_SIMPLEX
-    position = (50, 70)
-    font_scale = 0.5
-    font_color = (0, 0, 0)
-    line_type = 2
-
-    # Calculate text size
-    (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)    
-
-    # Adjust font scale if text width exceeds the rectangle width
-    while text_width > width - 20:  # Subtracting 20 for some padding
-        font_scale -= 0.1  # Decrease font scale
-        (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)
-        if font_scale <= 0.1:  # Prevent font scale from becoming too small or negative
-            break
-
-    # Calculate the center position
-    text_x = (width - text_width) // 2
-    text_y = top_left_corner[1] + ((bottom_right_corner[1] - top_left_corner[1]) + text_height) // 2
-
-    # The position where the text will start
-    position = (text_x, text_y)
-
-    # Put the text on the frame
-    cv.putText(frame, text, position, font, font_scale, font_color, line_type)
-    cv.imshow('Webcam', frame)
+from utils import *
     
     
 def webcam_inference():
@@ -110,13 +55,12 @@ def webcam_inference():
     cap.release()
     cv.destroyAllWindows()
     
-def video_inference():
+def video_inference(with_arduino=False):
     video_path = os.path.join('data','videos','light_left_feet_left_goal_10_goals_ok','lf13.mp4')
-    frame_start,frame_end = 0,5
+    frame_start,frame_end = 10,17
     best_model_path = os.path.join('models', 'best_train_2.pt')
     model = YOLO(best_model_path)
     cap = cv.VideoCapture(video_path)
-    frame_number = 0
     
     # Get the video codec and properties from the input video 
     fps = int(cap.get(cv.CAP_PROP_FPS))
@@ -124,13 +68,15 @@ def video_inference():
     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
     frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
     total_duration = frame_count / fps 
+    color = COLOR_BLACK
     print(f"Video (sec): {0}-{round(total_duration,2)}")
     print(f"Frames: {0}-{frame_count}")
     i = 0
-    while(True):
+    # frame_number = 0
+    # while(True):
     
-    # for i in range(frame_start,frame_end):
-        # cap.set(cv.CAP_PROP_POS_FRAMES, i)
+    for i in range(frame_start,frame_end):
+        cap.set(cv.CAP_PROP_POS_FRAMES, i)
         ret, frame= cap.read()
         if not ret: break
         
@@ -145,12 +91,14 @@ def video_inference():
         
         # plot frame
         frame_time = int(cap.get(cv.CAP_PROP_POS_MSEC))
-        frame_number+=1
-        i+=1
+        # frame_number+=1
+        # i+=1
         classes_probs_text = f"No ({round((classes_probs['no']*100.0),2)}%) - Yes ({round((classes_probs['yes']*100.0),2)}%)" 
-        text = f"Frame {frame_number}/{frame_count} - Time {round(frame_time,5)} ms - {classes_probs_text}"
-        text = f"{classes_probs_text}"
-        plot_frame(frame,width,height,text)
+        text = f"Frame {i}/{frame_count} - Time {round(frame_time,5)} ms - {classes_probs_text}"
+        if (classes_probs['yes'] > 0.5): 
+            color=COLOR_BLUE # red in practice
+            if(with_arduino): green_led_on()
+        plot_frame(frame,width,height,text,color)
         cv.waitKey(0)
     cap.release()
     cv.destroyAllWindows()
@@ -182,13 +130,22 @@ def image_inference():
     print("Results : ", classes_probs)
     cv.destroyAllWindows() 
 
-def arduino_inference():
+def arduino_webcam_inference():
     global ser
     ser = serial.Serial('COM3', 9600, timeout=1)
     time.sleep(2) # Wait for the connection to be established
     webcam_inference()
     # blink()
     # time.sleep(2)
+    ser.close()
+    print('end of inference')
+    
+def arduino_video_inference():
+    global ser
+    ser = serial.Serial('COM3', 9600, timeout=1)
+    time.sleep(2) # Wait for the connection to be established
+    green_led_off()
+    video_inference(with_arduino=True)
     ser.close()
     print('end of inference')
     
---
+++ utils.py
@@ -0,0 +1,99 @@
+from IPython.display import display
+import matplotlib.pyplot as plt
+from PIL import Image
+from ultralytics import YOLO
+import cv2 as cv
+import keyboard # get the keys typed
+import os, serial, time
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_BLUE = (0, 0, 255)
+
+def green_led_on(): ser.write(b'G')
+def green_led_off(): ser.write(b'g')
+def yellow_led_on(): ser.write(b'Y')
+def yellow_led_off(): ser.write(b'y')
+def blink():
+    print("yes detected")
+    green_led_off()
+    yellow_led_on()
+    time.sleep(2)
+    yellow_led_off()
+    green_led_on()
+        
+def plot_frame(frame,width,height,text,font_color=COLOR_BLACK):
+    # Define the position and size of the rectangle
+    top_left_corner = (0, 0)
+    bottom_right_corner = (width, 100)
+    rectangle_color = (255, 255, 255)  # White color in BGR
+    rectangle_thickness = -1  # Filled rectangle
+
+    # Draw the white rectangle
+    cv.rectangle(frame, top_left_corner, bottom_right_corner, rectangle_color, rectangle_thickness)
+
+    # Write text on the frame using OpenCV before converting it to RGB
+    font = cv.FONT_HERSHEY_SIMPLEX
+    position = (50, 70)
+    font_scale = 0.5
+    line_type = 2
+
+    # Calculate text size
+    (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)    
+
+    # Adjust font scale if text width exceeds the rectangle width
+    while text_width > width - 20:  # Subtracting 20 for some padding
+        font_scale -= 0.1  # Decrease font scale
+        (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)
+        if font_scale <= 0.1:  # Prevent font scale from becoming too small or negative
+            break
+
+    # Calculate the center position
+    text_x = (width - text_width) // 2
+    text_y = top_left_corner[1] + ((bottom_right_corner[1] - top_left_corner[1]) + text_height) // 2
+
+    # The position where the text will start
+    position = (text_x, text_y)
+
+    # Put the text on the frame
+    cv.putText(frame, text, position, font, font_scale, font_color, line_type)
+    cv.imshow('Webcam', frame)
+    
+def plot_frame_notebook(frame,width,height,text,font_color=COLOR_BLACK):
+    # Define the position and size of the rectangle
+    top_left_corner = (0, 0)
+    bottom_right_corner = (width, 100)
+    rectangle_color = (255, 255, 255)  # White color in BGR
+    rectangle_thickness = -1  # Filled rectangle
+
+    # Draw the white rectangle
+    cv.rectangle(frame, top_left_corner, bottom_right_corner, rectangle_color, rectangle_thickness)
+
+    # Write text on the frame using OpenCV before converting it to RGB
+    font = cv.FONT_HERSHEY_SIMPLEX
+    position = (50, 70)
+    font_scale = 1.5
+    line_type = 2
+
+    # Calculate text size
+    (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)    
+
+    # Adjust font scale if text width exceeds the rectangle width
+    while text_width > width - 20:  # Subtracting 20 for some padding
+        font_scale -= 0.1  # Decrease font scale
+        (text_width, text_height), _ = cv.getTextSize(text, font, font_scale, line_type)
+        if font_scale <= 0.1:  # Prevent font scale from becoming too small or negative
+            break
+
+    # Calculate the center position
+    text_x = (width - text_width) // 2
+    text_y = top_left_corner[1] + ((bottom_right_corner[1] - top_left_corner[1]) + text_height) // 2
+
+    # The position where the text will start
+    position = (text_x, text_y)
+    # Put the text on the frame
+    cv.putText(frame, text, position, font, font_scale, font_color, line_type)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(frame)
+    plt.axis('off')
+    plt.show()
